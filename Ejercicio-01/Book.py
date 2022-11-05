384c42d016fbe24d6c3527c4a7bb5d5f8540ab8b
jjnietoc <juanjonietocastillo@gmail.com>
2022-11-05 10:57:29 -0500

    Cambiar clase, modificar archivo funciones para que no se crucen, modificar main para que use switch case en vez de elif, else

--- Ejercicio-01/Book.py
+++ Ejercicio-01/Book.py
@@ -1,3 +1,4 @@
+
 class BookTest:
   def __init__(self, data):
     self.title = data.get('title', None)
@@ -8,4 +9,34 @@ class BookTest:
 
   def __str__(self):
     return f'El libro {self.title}, fue escrito por {self.author}.\nEs del genero {self.genres} y fue publicado por {self.publisher}'
+"""
 
+class Book:
+    def init (self):
+        self.title: str = None
+        self.genre: str = None
+        self.isbn: str = None
+        self.publisher: str = None
+        self.authors: str = None
+
+    def set_title(self, title_book: str):
+        self.title = title_book
+
+    def set_genre(self, genre_book: str):
+        self.genre = genre_book
+
+    def set_isbn(self, isbn_book: str):
+        self.isbn = isbn_book
+
+    def set_publisher(self, publisher_book: str):
+        self.publisher = publisher_book
+
+    def set_authors (self, authors_book: str):
+        self.authors = authors_book
+
+    def book_to_dictionary (self):
+        return vars(self)
+
+    #def __str__(self):
+     #   return f'El libro {self.title}, fue escrito por {self.author}.\nEs del genero {self.genres} y fue publicado por {self.publisher}'
+"""
--- Ejercicio-01/main.py
+++ Ejercicio-01/main.py
@@ -1,1 +1,28 @@
- 
+if __name__ == "__main__":
+
+    from funciones_jj import *
+    from Book import *
+
+    print("Bienvenid@!\nEste programa te permite:\n1. Listar libros\n2. Agregar libros\n3. Eliminar libros\n4. Buscar libros\n5. Actualizar libros\n6. Guardar libros")
+    print("---------------")
+    file1 = input("Ingrese el path del archivo que desea utilizar: ")
+    answer = int(input("Ingrese el numero de lo que desea hacer: "))
+    print("---------------")
+
+
+    match answer:
+        case 1:
+            list_books(file1)
+        case 2:
+            pass
+        case 3:
+            pass
+        case 4:
+            search_by(file1)
+        case 5:
+            pass
+        case 6:
+            pass
+        case default:
+            print("Por favor ingresa un numero de las opciones dadas.")
+
