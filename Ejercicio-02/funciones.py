743b11b63525607ef0c8a4f45c799a631107e3ab
jjnietoc <juanjonietocastillo@gmail.com>
2022-11-06 17:50:08 -0500

    Add funcion buscar por generacion y template menu

--- Ejercicio-02/funciones.py
+++ Ejercicio-02/funciones.py
@@ -1,1 +1,28 @@
 # Funciones para las búsquedas de pokemon
+
+import requests
+import pprint as pp
+
+#buscar pokemon por generacion
+def pkm_by_gen():
+    name_or_id = input("Que generacion quieres ver? ")  
+
+    url = "https://pokeapi.co/api/v2/generation/{}/".format(name_or_id)
+
+    response = requests.get(url)
+
+    if response.status_code != 200: 
+        print(response.text)
+    else:
+        data = response.json()
+        
+        print('\n--- data.keys() ---\n')
+        print(data.keys())
+        
+        print('\n--- Nombre de la generacion ---\n')
+        print(data['name'])
+
+        print('\n--- Pokemones en esta generacion ---\n')
+        pp.pprint(data["pokemon_species"])
+
+
--- Ejercicio-02/main.py
+++ Ejercicio-02/main.py
@@ -1,1 +1,21 @@
 # Archivo principal donde realiza la interaccion con el usuario
+
+if __name__ == "__main__":
+    from funciones import *
+
+    print("Bienvenid@!\nEste programa te permite:\n1. Listar pokemones por generacion \n2. Listar pokemones por forma\n3. Listar pokemones por habitat\n4. Listar pokemones por tipo")
+    print("---------------")
+    answer = int(input("Ingrese el numero de lo que desea hacer: "))
+    print("---------------")
+    
+
+    if answer ==  1:
+        pkm_by_gen()
+    elif answer == 2:
+        pass
+    elif answer == 3:
+        pass
+    elif answer == 4:
+        pass
+    else:
+        print('Por favor coloque una de las opciones disponibles.')
