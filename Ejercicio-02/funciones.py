6452361172928f6763eba8721e7a7bab7615fcd8
Jhonny Quiliche <jhonny.jql1210@gmail.com>
2022-11-06 19:18:58 -0500

    ADD: Busqueda por forma de pokemon

--- Ejercicio-02/funciones.py
+++ Ejercicio-02/funciones.py
@@ -26,3 +26,59 @@ def pkm_by_gen():
         pp.pprint(data["pokemon_species"])
 
 
+def pkm_by_form() -> None:
+
+  status = True
+
+  print('Que forma de pokemon quieres ver?\n Puede ingresar valores como 1, 2, 3, etc o también el nombre directo del pokemon como bulbasaur, charmander, squirtle, etc \n')
+
+  while status:
+
+    name_or_id = input('Ingrese su respuesta: ')  
+
+    url = f"https://pokeapi.co/api/v2/pokemon-form/{name_or_id}/"
+
+    response = requests.get(url)
+
+    if response.status_code != 200: 
+
+      print(response.text)
+      print('El valor o pokemon ingresado no es correcto')
+
+    else:
+
+      status = False
+      data = response.json()
+      
+      pokemon_name = data['pokemon']['name']
+      pokemon_image = data['sprites']['front_default']
+
+      pokemon_url = data['pokemon']['url']
+
+      resp = requests.get(pokemon_url)
+
+      if resp.status_code != 200: 
+        print(resp.text)
+
+      else:
+        new_data = resp.json()
+
+        pokemon_ability = []
+
+        abilities = new_data['abilities']
+
+        for ability in abilities:
+          pokemon_ability.append(ability['ability']['name'])
+      
+      print('\n--- Nombre de pokemon ---\n')
+      print(pokemon_name)
+      
+      print('\n--- Habilidades ---\n')
+
+      for ability in pokemon_ability:
+        print(ability)
+      
+      print('\n--- Url de imagen ---\n')
+      print(pokemon_image)
+
+
